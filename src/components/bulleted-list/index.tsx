6369a4b61487cc45dc7e146d83f1e9b579073ea1
Bogdan Aksenov <b.aksenov@yadro.com>
2023-02-28 00:05:22 +0300

    Optimize marker

--- src/components/bulleted-list/index.tsx
+++ src/components/bulleted-list/index.tsx
@@ -2,33 +2,17 @@ import React from "react"
 import styled from "styled-components"
 
 const List = styled.ul`
-  list-style: none;
+  padding-left: 20px;
+  list-style-type: circle;
 `
 
 const Item = styled.li`
   color: #37352f;
-  display: flex;
   flex-direction: row;
   margin-top: 0.4em;
   min-height: 30px;
   align-items: flex-start;
-`
-
-const Bullet = styled.div`
-  display: flex;
-  min-width: 30px;
-  min-height: 30px;
-  user-select: none;
-  margin-right: 2px;
-  font-size: 1.5em;
-  line-height: 1;
-  align-items: center;
-  justify-content: center;
-
-  ::before {
-    content: "•";
-    font-family: Arial;
-  }
+  list-style-type: disc;
 `
 
 interface BulletedListProps extends React.HTMLAttributes<HTMLUListElement> {
@@ -39,10 +23,7 @@ export const BulletedList = ({ children, ...props }: BulletedListProps) => {
   return (
     <List {...props}>
       {children?.map((item, index) => (
-        <Item key={index}>
-          <Bullet />
-          {item}
-        </Item>
+        <Item key={index}>{item}</Item>
       ))}
     </List>
   )
--- src/components/numbered-list/index.tsx
+++ src/components/numbered-list/index.tsx
@@ -1,35 +1,20 @@
 import React from "react"
 import styled from "styled-components"
 
-const List = styled.ul`
-  list-style: none;
+const List = styled.ol`
+  padding-left: 20px;
+  list-style-type: decimal;
 `
 
 const Item = styled.li`
   color: #37352f;
-  display: flex;
   flex-direction: row;
   margin-top: 0.4em;
   min-height: 30px;
   align-items: flex-start;
-`
 
-const Number = styled.div`
-  display: flex;
-  min-width: 30px;
-  min-height: 30px;
-  user-select: none;
-  margin-right: 2px;
-  font-size: 1.5em;
-  line-height: 1;
-  align-items: center;
-  justify-content: center;
-
-  ::before {
-    content: "${(props: { value: number }) => props.value}.";
-    font-family: "SegoeUI", sans-serif;
-    -webkit-font-smoothing: auto;
-    font-size: 16px;
+  ::marker {
+    font-family: "Segoe UI", sans-serif;
   }
 `
 
@@ -41,10 +26,7 @@ export const NumberedList = ({ children, ...props }: NumberedListProps) => {
   return (
     <List {...props}>
       {children?.map((item, index) => (
-        <Item key={index}>
-          <Number value={index + 1} />
-          {item}
-        </Item>
+        <Item key={index}>{item}</Item>
       ))}
     </List>
   )
